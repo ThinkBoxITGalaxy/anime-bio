4e36117619ffdcf209e6acfd1af60281cf9d1a20
jee ar <57131696+w33ing@users.noreply.github.com>
2021-05-07 14:24:28 +0800

    Add files via upload

---
+++ timeconsole/timeconsole/Connz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace timeconsole
+{
+    class Connz
+    {
+        public static SqlConnection sqlConnz
+        {
+            get
+            {
+                string constring = "Server=DESKTOP-UJBAK2U; user id=anime; password=123; database=anime_db";
+                return new SqlConnection(constring);
+            }
+        }
+    }
+}
